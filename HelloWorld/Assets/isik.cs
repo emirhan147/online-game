dd9794e4ccd4e533ed9fbffbd8124bba04725012
emirhan147 <57144080+emirhan147@users.noreply.github.com>
2020-05-05 23:31:29 +0300

    NewGame1

---
+++ HelloWorld/Assets/isik.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isik : MonoBehaviour
+{
+    // Start is called before the first frame update
+    RaycastHit nesne;
+    public GameObject hedef;
+    public float Health;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Shoot();
+            if (Health == 0)
+            {
+                Destroy(hedef);
+            }
+        }
+    }
+    void Shoot()
+    {
+        
+            if (Physics.Raycast(transform.position, transform.forward, out nesne, 10))
+            {
+            print("shooted");
+            
+            Health -= 10;
+            }
+
+            
+        }
+        
+        
+    }
+
